33091d070343c175bc79c30aad0c6b7fd8bdf41d
tyrocca <tyler.rocca@gmail.com>
2018-01-07 17:37:05 -0500

    making models

--- emails/models.py
+++ emails/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class State(models.Model):
+    """
+    Model to describe a state
+    """
+    name = models.CharField(max_length=128)
+    abbrev = models.CharField(max_length=2)
+    capital = models.CharField(max_length=128)
+    fips_code = models.SmallIntegerField()
+
+
+class City(models.Model):
+    """
+    This model describes a city
+    """
+    city_name = models.CharField(max_length=255)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+
+
+class Subscriber(models.Model):
+    """
+    This database model describes the subscriber
+    """
+    email = models.EmailField()
+    is_valid = models.BooleanField()
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
--- simple_weather_app/settings.py
+++ simple_weather_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # User added apps
+    'emails.apps.EmailsConfig',
 ]
 
 MIDDLEWARE = [
